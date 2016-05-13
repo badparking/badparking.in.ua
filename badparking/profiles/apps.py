1eb9576a540ae8841ba95d08f3a9b14b4ce03330
Artem Hluvchynskyi <excieve@gmail.com>
2016-05-13 22:46:54 +0300

    Implements BankID integration for Oschadbank and Privatbank, closes #2

--- badparking/profiles/apps.py
+++ badparking/profiles/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from .bankid import OschadBankId, PrivatBankId
 
 
 class ProfilesConfig(AppConfig):
     name = 'profiles'
+
+    def ready(self):
+        # Load BankID clients once on app load
+        if not hasattr(self, 'oschad_bankid'):
+            self.oschad_bankid = OschadBankId(**settings.BANKID_OSCHADBANK)
+        if not hasattr(self, 'privat_bankid'):
+            self.privat_bankid = PrivatBankId(**settings.BANKID_PRIVATBANK)
